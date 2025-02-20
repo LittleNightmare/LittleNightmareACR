5596648e2ebab0809e8dd10533efbe8170f49c00
LittleNightmare <littlenightmare2000@gmail.com>
2025-02-20 12:14:25 +0800

    修复7.1报错问题 fix #2
    火神冲尚未同步7.1的更改，可能出现逻辑问题

--- LNMHelper.cs
+++ LNMHelper.cs
@@ -5,7 +5,7 @@ using AEAssist.MemoryApi;
 using FFXIVClientStructs.FFXIV.Client.Game.Event;
 using FFXIVClientStructs.FFXIV.Client.Game.UI;
 using FFXIVClientStructs.STD;
-using Lumina.Excel.GeneratedSheets;
+using Lumina.Excel.Sheets;
 
 namespace LittleNightmare;
 
@@ -18,10 +18,12 @@ public class LNMHelper
         if (target == null) return false;
         var additionalCheck = target.IsBoss();
         // TODO: 多变迷宫也是四人本，所以用不了这个判断，要不判断CountPoint>=1？
+        var dutyInfo = Core.Resolve<MemApiDuty>().DutyInfo;
         if (Core.Resolve<MemApiDuty>().DutyMembersNumber() == 24
             // 这个是迷宫挑战，这两个应该都可以用这个代替IsBoss
             // 2是迷宫挑战
-            || Core.Resolve<MemApiDuty>().DutyInfo.ContentType.Row == 2)
+            || dutyInfo is { ContentType.Value.RowId: 2}
+            )
             additionalCheck = Core.Resolve<MemApiDuty>().InBossBattle;
         return IsLastDutyTask() && additionalCheck;
     }
@@ -48,7 +50,7 @@ public class LNMHelper
             currentCount += task.CountCurrent;
             // 处理那种存在第一行任务进行度，第二行显示当前任务的情况
             var instanceContentTextData = ECHelper.Data.GetExcelSheet<InstanceContentTextData>()?.GetRow(7);
-            if (instanceContentTextData != null && !task.Label.IsEmpty && task.Label.ToString().Equals(instanceContentTextData.Text.RawString)) break;
+            if (instanceContentTextData != null && !task.Label.IsEmpty && task.Label.ToString().Equals(instanceContentTextData.Value.Text.ExtractText())) break;
         }
 
         if (totalNeeded == 0) return true;
